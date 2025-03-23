d8f59817b169b641bb6ccd8d798f11c36370cda7
Tyler <tyler@tylergraham.com>
2025-03-23 11:07:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,173 +1,150 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = 800;
-const height = canvas.height = 600;
+const width = canvas.width;
+const height = canvas.height;
 
-let lastTime = 0;
-let asteroidInterval = 2000; // Interval in milliseconds for asteroid generation
-let nextAsteroidTime = 0;
+let paddleSpeed = 7;
+let ballSpeed = 5;
+let ballRadius = 10;
+let paddleWidth = 100;
+let paddleHeight = 10;
+let brickRowCount = 5;
+let brickColumnCount = 8;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
 
-class Bullet {
-    constructor(x, y, angle) {
-        this.x = x;
-        this.y = y;
-        this.angle = angle;
-        this.speed = 10;
+let paddle = {
+    x: (width - paddleWidth) / 2,
+    y: height - 30,
+    width: paddleWidth,
+    height: paddleHeight,
+    dx: 0
+};
+
+let ball = {
+    x: Math.random() * (width - 2 * ballRadius) + ballRadius,
+    y: Math.random() * (height / 2),
+    dx: (Math.random() < 0.5 ? -1 : 1) * ballSpeed,
+    dy: -ballSpeed,
+    radius: ballRadius
+};
+
+let bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
+}
 
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 3, 0, Math.PI * 2);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        ctx.closePath();
-    }
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.closePath();
+}
 
-    update() {
-        this.x += Math.cos(this.angle) * this.speed;
-        this.y += Math.sin(this.angle) * this.speed;
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.closePath();
+}
 
-        if (this.x < 0 || this.x > width || this.y < 0 || this.y > height) {
-            this.remove = true;
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = 'red';
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
 
-class Asteroid {
-    constructor() {
-        this.x = Math.random() * width;
-        this.y = Math.random() * height;
-        this.size = Math.random() * 50 + 50;
-        this.speedX = (Math.random() - 0.5) * 2;
-        this.speedY = (Math.random() - 0.5) * 2;
+function updateBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    if (ball.x + ball.radius > width || ball.x - ball.radius < 0) {
+        ball.dx = -ball.dx;
     }
-
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.strokeStyle = 'white';
-        ctx.stroke();
+    if (ball.y - ball.radius < 0) {
+        ball.dy = -ball.dy;
     }
-
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-
-        if (this.x < 0) this.x = width;
-        if (this.x > width) this.x = 0;
-        if (this.y < 0) this.y = height;
-        if (this.y > height) this.y = 0;
+    if (ball.y + ball.radius > height) {
+        if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
+            ball.dy = -ball.dy;
+        } else {
+            // Game over
+            alert('Game Over');
+            document.location.reload();
+        }
     }
 }
 
-const ship = {
-    x: width / 2,
-    y: height / 2,
-    angle: 0,
-    rotationSpeed: 0.05,
-    thrust: {
-        x: 0,
-        y: 0
-    },
-    draw() {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.angle);
-        ctx.strokeStyle = 'white';
-        ctx.lineWidth = 2;
-        ctx.beginPath();
-        ctx.moveTo(10, 0);
-        ctx.lineTo(-10, -7);
-        ctx.lineTo(-5, 0);
-        ctx.lineTo(-10, 7);
-        ctx.closePath();
-        ctx.stroke();
-        ctx.restore();
-    },
-    update() {
-        this.x += this.thrust.x;
-        this.y += this.thrust.y;
+function updatePaddle() {
+    paddle.x += paddle.dx;
 
-        if (this.x > width) this.x = 0;
-        if (this.x < 0) this.x = width;
-        if (this.y > height) this.y = 0;
-        if (this.y < 0) this.y = height;
+    if (paddle.x < 0) {
+        paddle.x = 0;
+    } else if (paddle.x + paddle.width > width) {
+        paddle.x = width - paddle.width;
     }
-};
+}
 
-let bullets = [];
-let asteroids = [];
-
-function generateAsteroid() {
-    const asteroid = new Asteroid();
-    asteroids.push(asteroid);
+function checkCollision() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            let brick = bricks[c][r];
+            if (brick.status === 1) {
+                if (ball.x > brick.x && ball.x < brick.x + brickWidth && ball.y > brick.y && ball.y < brick.y + brickHeight) {
+                    ball.dy = -ball.dy;
+                    brick.status = 0;
+                }
+            }
+        }
+    }
 }
 
 function gameLoop(timestamp) {
-    const deltaTime = timestamp - lastTime;
-    lastTime = timestamp;
-
-    if (timestamp > nextAsteroidTime) {
-        generateAsteroid();
-        nextAsteroidTime = timestamp + asteroidInterval;
-    }
-
     ctx.clearRect(0, 0, width, height);
 
-    ship.update();
-    ship.draw();
-
-    bullets.forEach(bullet => {
-        bullet.draw();
-        bullet.update();
-    });
-
-    asteroids.forEach(asteroid => {
-        asteroid.draw();
-        asteroid.update();
-    });
-
-    // Collision detection
-    bullets.forEach(bullet => {
-        asteroids.forEach(asteroid => {
-            const dx = bullet.x - asteroid.x;
-            const dy = bullet.y - asteroid.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance < asteroid.size) {
-                bullet.remove = true;
-                asteroid.remove = true;
-            }
-        });
-    });
-
-    // Clean up bullets and asteroids
-    bullets = bullets.filter(bullet => !bullet.remove);
-    asteroids = asteroids.filter(asteroid => !asteroid.remove);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    updateBall();
+    updatePaddle();
+    checkCollision();
 
     requestAnimationFrame(gameLoop);
 }
 
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowUp') {
-        ship.thrust.x += Math.cos(ship.angle) * 0.1;
-        ship.thrust.y += Math.sin(ship.angle) * 0.1;
-    } else if (e.key === 'ArrowLeft') {
-        ship.angle -= ship.rotationSpeed;
+    if (e.key === 'ArrowLeft') {
+        paddle.dx = -paddleSpeed;
     } else if (e.key === 'ArrowRight') {
-        ship.angle += ship.rotationSpeed;
-    } else if (e.key === 'Space') {
-        const bullet = new Bullet(ship.x, ship.y, ship.angle);
-        bullets.push(bullet);
+        paddle.dx = paddleSpeed;
     }
 });
 
 document.addEventListener('keyup', (e) => {
-    if (e.key === 'ArrowUp') {
-        ship.thrust.x = 0;
-        ship.thrust.y = 0;
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+        paddle.dx = 0;
     }
 });
 
-requestAnimationFrame(gameLoop);
+gameLoop();
